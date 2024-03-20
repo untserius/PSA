576367c8cb32b3a8804c991a3ffe368e8515b151
Sudhir Das <sudhirdas1999@gmail.com>
2024-03-20 11:21:13 +0530

    created roles

--- STS Project/blogapp/src/main/java/com/blogapp/entity/User.java
+++ STS Project/blogapp/src/main/java/com/blogapp/entity/User.java
@@ -2,10 +2,8 @@ package com.blogapp.entity;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Set;
 
 @Data
 @Entity
@@ -17,4 +15,13 @@ public class User {
     private String email;
     private String username;
     private String password;
+
+    @ManyToMany (fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
+    )
+    private Set<Role> roles;
+//    List<Role> roles = new ArrayList<>();
 }
